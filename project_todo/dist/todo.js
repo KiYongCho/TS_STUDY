eab3cb0bd863eee99bb91d07c34bfae5f9b7e693
realchoky <realchoky@gmail.com>
2024-01-20 18:15:47 +0900

    123

--- project_todo/dist/todo.js
+++ project_todo/dist/todo.js
@@ -1,5 +1,7 @@
+// 배열로 정의
 const todos = [
     { id: 1, title: '아침먹기', completed: true },
     { id: 2, title: 'TS공부', completed: true },
     { id: 3, title: '점심먹기', completed: false }
 ];
+// JSON
