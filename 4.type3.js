48d39e031e6b7a38ed1d2009576664e1d4eb79c6
realchoky <realchoky@gmail.com>
2024-01-25 22:25:01 +0900

    123

--- 4.type3.js
+++ 4.type3.js
@@ -46,10 +46,63 @@ console.log(number, typeof number); // 10 number
 console.log();
 function shuffleBooks(books) {
     // const result = books!.shuffle(); // 실행시 에러
-    // const result = books?.shuffle(); // undefined
+    var result = books === null || books === void 0 ? void 0 : books.shuffle(); // undefined
     return result;
 }
 // 타입체크는 오케이, 실행시 에러 , 실행시에도 에러 안내려면 !를 ?로
 console.log(shuffleBooks(null));
 console.log();
 /* 타입 가드 (type guard) */
+// 여러개의 타입으로 지정된 값을 특정 위치에서 원하는 값으로 구분하는 것
+// typeof, instanceof, in 연산자를 사용해 타입범위를 좁히는 것
+// 3가지 타입 중 2가지는 가드(막는다)
+var var1 = 'hello';
+if (typeof var1 === 'string') {
+    console.log(var1.toUpperCase());
+}
+console.log();
+// 타입가드를 위해 타입단언을 사용하게 되면 실행시 오류를 피할 수 없다
+function func1(param1) {
+    console.log(param1.toFixed(2));
+}
+func1(3.1415);
+// func1('hello'); // error
+console.log();
+function Bird(name) {
+    this.name = name;
+}
+function Mammal(name) {
+    this.name = name;
+}
+var bird = new Bird('독수리');
+if (bird instanceof Bird) {
+    console.log(bird.name);
+}
+// if (bird instanceof Mammal) {
+//     console.log(bird.name);
+// }
+console.log();
+var book = { name: 'typescript', author: '마소' };
+if ('author' in book) {
+    console.log(book);
+}
+function isHongOrPark(someone) {
+    return someone.age !== undefined;
+}
+var designer = {
+    name: '홍길순',
+    age: 20,
+    jobName: 'designer'
+};
+var programmer = {
+    name: '홍길동',
+    age: 30,
+    jobName: 'programmer'
+};
+if (designer.jobName === 'designer') {
+    console.log(designer.jobName);
+}
+if (programmer.jobName === 'programmer') {
+    console.log(programmer.jobName);
+}
+console.log();
--- 4.type3.ts
+++ 4.type3.ts
@@ -72,7 +72,7 @@ interface Books {
 }
 function shuffleBooks(books: Books | null) {
     // const result = books!.shuffle(); // 실행시 에러
-    // const result = books?.shuffle(); // undefined
+    const result = books?.shuffle(); // undefined
     return result;
 }
 // 타입체크는 오케이, 실행시 에러 , 실행시에도 에러 안내려면 !를 ?로
@@ -82,6 +82,118 @@ console.log();
 
 
 /* 타입 가드 (type guard) */
+// 여러개의 타입으로 지정된 값을 특정 위치에서 원하는 값으로 구분하는 것
+// typeof, instanceof, in 연산자를 사용해 타입범위를 좁히는 것
+
+// 3가지 타입 중 2가지는 가드(막는다)
+const var1: string|number|boolean = 'hello';
+if (typeof var1 === 'string') {
+    console.log(var1.toUpperCase());    
+}
+
+console.log();
+
+// 타입가드를 위해 타입단언을 사용하게 되면 실행시 오류를 피할 수 없다
+function func1(param1: string|number|boolean): void {
+    console.log((param1 as number).toFixed(2));
+}
+func1(3.1415);
+// func1('hello'); // error
+
+console.log();
+
+// instanceof를 활용한 객체타입의 타입가드
+interface Bird {
+    name: string;
+}
+function Bird(name: string): void {
+    this.name = name;
+}
+interface Mammal {
+    name: string;
+}
+function Mammal(name: string): void {
+    this.name = name;
+}
+const bird: Bird = new Bird('독수리');
+if (bird instanceof Bird) {
+    console.log(bird.name);
+}
+// if (bird instanceof Mammal) {
+//     console.log(bird.name);
+// }
+
+console.log();
+
+// in 연산자를 활용한 타입가드
+interface Book {
+    name: string;
+    author: string;
+}
+interface Lecture {
+    name: string;
+    tutor: string;
+}
+const book: Book = {name: 'typescript', author: '마소'};
+if ('author' in book) {
+    console.log(book);    
+}
+// if ('tutor' in book) {
+//     console.log(book);    
+// }
+
+// 타입가드 함수 (is 사용)
+// 두개 이상의 타입이 같은 프라퍼티를 가지고 있으면 in 연산자로
+// 타입가드가 불가능 하므로 그럴때는 타입가드 함수를 사용해야 함
+interface Hong {
+    name: string;
+    age: number;
+}
+interface Park {
+    name: string;
+    address: string;
+}
+function isHongOrPark(someone: Hong | Park): someone is Hong {
+    return (someone as Hong).age !== undefined;
+}
+
+// 구별된유니언타입 (discriminated unions)
+// 두개 이상의 타입이 같은 이름의 프라퍼티만 가지고 있을때 프라퍼티의 값으로 타입가드
+interface Designer {
+    name: string,
+    age: number,
+    jobName: 'designer'
+}
+const designer: Designer = {
+  name: '홍길순',
+  age: 20,
+  jobName: 'designer'  
+};
+interface Programmer {
+    name: string,
+    age: number,
+    jobName: 'programmer'
+}
+const programmer: Programmer = {
+    name: '홍길동',
+    age: 30,
+    jobName: 'programmer'  
+  };
+  if (designer.jobName==='designer') {
+    console.log(designer.jobName);
+}
+if (programmer.jobName==='programmer') {
+    console.log(programmer.jobName);
+}
+
+console.log();
+
+
+
+
+
+
+
 
 
 
